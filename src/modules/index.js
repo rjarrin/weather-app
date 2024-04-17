072ecafceab776e39e6e485612061e2299c8773a
rjarrin <jarrin.ricardo@gmail.com>
2024-04-17 16:59:11 -0400

    Footer updated

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,4 @@
 import '../style.css';
 import generateHeader from './dom';
-import { fetchForecastData } from './api';
 
 generateHeader();
-
-console.log(`index.js - fetchForecastData:`);
-console.log(fetchForecastData("Toronto"));
